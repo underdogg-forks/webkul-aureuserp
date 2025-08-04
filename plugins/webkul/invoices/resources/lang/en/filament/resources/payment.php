8a5ba594a5cf7335623f1e27c42e8cbd46372dad
vipin-yadav-webkul <vipinyadav.akeneo432@webkul.in>
2025-08-04 18:02:19 +0530

    Issue #543 has been fixed.

---
+++ plugins/webkul/invoices/resources/lang/en/filament/resources/payment.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'form' => [
+        'sections' => [
+            'fields' => [
+                'vender'                => 'Vender',
+                'vender-bank-account'   => 'Vender Bank Account',
+            ],
+        ],
+    ],
+];
