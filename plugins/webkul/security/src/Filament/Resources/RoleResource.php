a0a65ad9f076a7f7e30dc417720d3b0918f4d3bf
Suraj Kashyap <suraj.kashyap370@webkul.in>
2025-08-14 16:58:51 +0530

    Refactor RoleResource table method to remove filters and add record selection check

--- plugins/webkul/security/src/Filament/Resources/RoleResource.php
+++ plugins/webkul/security/src/Filament/Resources/RoleResource.php
@@ -106,9 +106,6 @@ class RoleResource extends Resource implements HasShieldPermissions
                     ->label(__('filament-shield::filament-shield.column.updated_at'))
                     ->dateTime(),
             ])
-            ->filters([
-                //
-            ])
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make()
@@ -117,7 +114,10 @@ class RoleResource extends Resource implements HasShieldPermissions
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ])
-            ->defaultSort('created_at', 'asc');
+            ->defaultSort('created_at', 'asc')
+            ->checkIfRecordIsSelectableUsing(
+                fn (Model $record): bool => $record->name != config('filament-shield.panel_user.name'),
+            );
     }
 
     public static function getRelations(): array
