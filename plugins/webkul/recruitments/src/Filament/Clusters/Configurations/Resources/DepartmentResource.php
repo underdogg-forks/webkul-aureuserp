5701d79c6a1441b7f1ba3de9f5cc52147d1de166
sandeepchawla <sandeep.chawla322@webkul.in>
2025-09-11 11:43:09 +0530

    made custom policy for duplicate resources like employement type, job position, skill type, department and add missing policy for packaging

--- plugins/webkul/recruitments/src/Filament/Clusters/Configurations/Resources/DepartmentResource.php
+++ plugins/webkul/recruitments/src/Filament/Clusters/Configurations/Resources/DepartmentResource.php
@@ -2,13 +2,14 @@
 
 namespace Webkul\Recruitment\Filament\Clusters\Configurations\Resources;
 
+use BezhanSalleh\FilamentShield\Contracts\HasShieldPermissions;
 use Filament\Tables\Table;
 use Webkul\Employee\Filament\Resources\DepartmentResource as BaseDepartmentResource;
 use Webkul\Recruitment\Filament\Clusters\Configurations;
 use Webkul\Recruitment\Filament\Clusters\Configurations\Resources\DepartmentResource\Pages;
 use Webkul\Recruitment\Models\Department;
 
-class DepartmentResource extends BaseDepartmentResource
+class DepartmentResource extends BaseDepartmentResource implements HasShieldPermissions
 {
     protected static ?string $model = Department::class;
 
@@ -22,6 +23,24 @@ class DepartmentResource extends BaseDepartmentResource
     public static function getSlug(): string
     {
         return 'departments';
+    }
+
+    public static function getPermissionPrefixes(): array
+    {
+        return [
+            'view::recruitment',
+            'view_any::recruitment',
+            'create::recruitment',
+            'update::recruitment',
+            'restore::recruitment',
+            'restore_any::recruitment',
+            'replicate::recruitment',
+            'reorder::recruitment',
+            'delete::recruitment',
+            'delete_any::recruitment',
+            'force_delete::recruitment',
+            'force_delete_any::recruitment',
+        ];
     }
 
     public static function table(Table $table): Table
