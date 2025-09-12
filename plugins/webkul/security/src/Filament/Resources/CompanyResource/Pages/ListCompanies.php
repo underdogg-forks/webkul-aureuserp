fa3acff2794bcdbff0dd62ca90fde8aa0ab0a296
Suraj Kashyap <suraj.kashyap370@webkul.in>
2025-09-12 11:30:37 +0530

    Add HasTableViews trait to ListCompanies for enhanced table functionality

--- plugins/webkul/security/src/Filament/Resources/CompanyResource/Pages/ListCompanies.php
+++ plugins/webkul/security/src/Filament/Resources/CompanyResource/Pages/ListCompanies.php
@@ -7,9 +7,12 @@ use Filament\Resources\Pages\ListRecords;
 use Filament\Schemas\Components\Tabs\Tab;
 use Webkul\Security\Filament\Resources\CompanyResource;
 use Webkul\Support\Models\Company;
+use Webkul\TableViews\Filament\Concerns\HasTableViews;
 
 class ListCompanies extends ListRecords
 {
+    use HasTableViews;
+
     protected static string $resource = CompanyResource::class;
 
     public function getTabs(): array
