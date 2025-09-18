379353ab3c7bcd8ba33398dbe76b2deed45c9022
sandeepchawla <sandeep.chawla322@webkul.in>
2025-09-18 15:48:08 +0530

    Added permissions for Employee and related resources in FilamentShield configuration and removed unused methods from Employee policies.

--- config/filament-shield.php
+++ config/filament-shield.php
@@ -250,6 +250,19 @@ return [
             \Webkul\Blog\Filament\Admin\Clusters\Configurations\Resources\TagResource::class      => ['view_any', 'create', 'update', 'delete', 'restore', 'delete_any', 'force_delete', 'force_delete_any', 'restore_any'],
             \Webkul\Blog\Filament\Admin\Resources\PostResource::class                             => ['view_any', 'view', 'create', 'update', 'delete', 'restore', 'delete_any', 'force_delete', 'force_delete_any', 'restore_any'],
 
+            \Webkul\Employee\Filament\Resources\EmployeeResource::class                               => ['view_any', 'view', 'create', 'update', 'delete', 'restore', 'delete_any', 'force_delete', 'force_delete_any'],
+            \Webkul\Employee\Filament\Resources\DepartmentResource::class                             => ['view_any', 'view', 'create', 'update', 'delete', 'restore', 'delete_any', 'force_delete', 'force_delete_any', 'restore_any'],
+
+            \Webkul\Employee\Filament\Clusters\Reportings\Resources\EmployeeSkillResource::class      => ['view_any', 'view'],
+
+            \Webkul\Employee\Filament\Clusters\Configurations\Resources\ActivityPlanResource::class          => ['view_any', 'view', 'create', 'update', 'delete', 'restore', 'delete_any', 'force_delete', 'force_delete_any', 'restore_any'],
+            \Webkul\Employee\Filament\Clusters\Configurations\Resources\CalendarResource::class              => ['view_any', 'view', 'create', 'update', 'delete', 'restore', 'delete_any', 'force_delete', 'force_delete_any', 'restore_any'],
+            \Webkul\Employee\Filament\Clusters\Configurations\Resources\DepartureReasonResource::class       => ['view_any', 'view', 'create', 'update', 'delete', 'delete_any', 'reorder'],
+            \Webkul\Employee\Filament\Clusters\Configurations\Resources\EmployeeCategoryResource::class      => ['view_any', 'view', 'create', 'update', 'delete', 'delete_any'],
+            \Webkul\Employee\Filament\Clusters\Configurations\Resources\WorkLocationResource::class          => ['view_any', 'view', 'create', 'update', 'delete', 'restore', 'delete_any', 'force_delete', 'force_delete_any', 'restore_any'],
+            \Webkul\Employee\Filament\Clusters\Configurations\Resources\SkillTypeResource::class             => ['view_any', 'view', 'create', 'update', 'delete', 'restore', 'delete_any', 'force_delete', 'force_delete_any', 'restore_any'],
+            \Webkul\Employee\Filament\Clusters\Configurations\Resources\EmploymentTypeResource::class        => ['view_any', 'view', 'create', 'update', 'delete', 'delete_any', 'reorder'],
+
         ],
         'exclude' => [
             \BezhanSalleh\FilamentShield\Resources\Roles\RoleResource::class,
--- plugins/webkul/employees/src/Policies/ActivityPlanPolicy.php
+++ plugins/webkul/employees/src/Policies/ActivityPlanPolicy.php
@@ -89,20 +89,4 @@ class ActivityPlanPolicy
     {
         return $user->can('restore_any_activity::plan');
     }
-
-    /**
-     * Determine whether the user can replicate.
-     */
-    public function replicate(User $user, ActivityPlan $activityPlan): bool
-    {
-        return $user->can('replicate_activity::plan');
-    }
-
-    /**
-     * Determine whether the user can reorder.
-     */
-    public function reorder(User $user): bool
-    {
-        return $user->can('reorder_activity::plan');
-    }
 }
